4c7fbac48b47a12455e5f5d2927ea6589d82cbb7
karma4u101 <petersson.peter@gmail.com>
2014-01-03 10:46:48 +0100

    Resolved #1. Adding a list-group menu example with collapsible sub menus to fobo-bs3-sandbox.

--- fobo-bs3-sandbox/build.sbt
+++ fobo-bs3-sandbox/build.sbt
@@ -4,21 +4,7 @@ version := "0.0.1-SNAPSHOT"
 
 organization := "net.liftweb"
 
-//version in ThisBuild := "1.1-SNAPSHOT"
-
-//liftVersion in ThisBuild <<= liftVersion ?? "2.6-SNAPSHOT"
-
-//liftEdition in ThisBuild <<= liftVersion apply { _.substring(0,3) }
-
-//name <<= (name, liftEdition) { (n, e) =>  n + "_" + e }
-
 crossScalaVersions := Seq("2.10.0", "2.9.3", "2.9.2", "2.9.1-1", "2.9.1")
-
-//scalaVersion in ThisBuild := "2.10.0"
-
-//EclipseKeys.withSource := true
-
-//EclipseKeys.skipParents in ThisBuild := false
 
 resolvers ++= Seq("snapshots"     at "http://oss.sonatype.org/content/repositories/snapshots",
                   "staging"       at "http://oss.sonatype.org/content/repositories/staging",
@@ -32,9 +18,9 @@ unmanagedResourceDirectories in Test <+= (baseDirectory) { _ / "src/main/webapp"
 scalacOptions ++= Seq("-deprecation", "-unchecked")
 
 libraryDependencies <++= (liftVersion,liftEdition,version) { (v,e,mv) =>
-    "net.liftweb"     %% "lift-webkit"            % v    % "compile" ::
-    "net.liftweb"     %% "lift-mapper"            % v    % "compile" ::
-    "net.liftmodules" %% ("fobo"+"_"+e)          % "1.2-SNAPSHOT" % "compile" ::
+    "net.liftweb"     %% "lift-webkit"     % v              % "compile" ::
+    "net.liftweb"     %% "lift-mapper"     % v              % "compile" ::
+    "net.liftmodules" %% ("fobo"+"_"+e)    % "1.2-SNAPSHOT" % "compile" ::
     Nil
 }
 
--- fobo-bs3-sandbox/src/main/scala/bootstrap/liftweb/Boot.scala
+++ fobo-bs3-sandbox/src/main/scala/bootstrap/liftweb/Boot.scala
@@ -50,6 +50,7 @@ class Boot {
     //see http://liftweb.net/lift_modules for more info
     FoBo.InitParam.JQuery=FoBo.JQuery191  
     FoBo.InitParam.ToolKit=FoBo.Bootstrap300 
+    FoBo.InitParam.ToolKit=FoBo.FontAwesome401
     FoBo.init() 
     
     //Show the spinny image when an Ajax call starts
@@ -90,11 +91,29 @@ class Boot {
     val userMenu   = User.AddUserMenusHere
     val static     = Menu(Loc("Static", Link(List("static"), true, "/static/index"), S.loc("StaticContent" , scala.xml.Text("Static Content")),LocGroup("lg2","topRight")))
     val twbs       = Menu(Loc("Bootstrap3", Link(List("bootstrap3"), true, "/bootstrap3/index"), S.loc("Bootstrap3" , scala.xml.Text("Bootstrap3")),LocGroup("lg2")))
-     
+
+    //list-group menu example with collapsible sub menus
+    val sbL1Dashbord      = Menu(Loc("sbL1Dashbord"     ,Link(List("sbL1Dashbord")     ,true, "#sbL1Dashbord")     , S.loc("sbL1Dashbord"     , Text("Dashboard"))       ))
+    val sbL1Users         = Menu(Loc("sbL1Users"        ,Link(List("sbL1Users")        ,true, "#users")            , S.loc("sbL1Users"        , Text("Users"))           ))
+    val sbL2Users         = Menu(Loc("sbL2Users"        ,Link(List("sbL2Users")        ,true, "#sbL2Users")        , S.loc("sbL2Users"        , Text("Users"))           ))
+    val sbL2CreateUsr     = Menu(Loc("sbL2CreateUsr"    ,Link(List("sbL2CreateUsr")    ,true, "#sbL2CreateUsr")    , S.loc("sbL2CreateUsr"    , Text("Create User"))     ))
+    val sbL2CreateGrp     = Menu(Loc("sbL2CreateGrp"    ,Link(List("sbL2CreateGrp")    ,true, "#sbL2CreateGrp")    , S.loc("sbL2CreateGrp"    , Text("Create Group"))    ))
+    val sbL1Articles      = Menu(Loc("sbL1Articles"     ,Link(List("sbL1Articles")     ,true, "#articles")         , S.loc("sbL1Articles"     , Text("Articles"))           ))
+    val sbL2Articles      = Menu(Loc("sbL2Articles"     ,Link(List("sbL2Articles")     ,true, "#sbL2Articles")     , S.loc("sbL2Articles"     , Text("Articles"))        ))
+    val sbL2CreateArticle = Menu(Loc("sbL2CreateArticle",Link(List("sbL2CreateArticle"),true, "#sbL2CreateArticle"), S.loc("sbL2CreateArticle", Text("Create Article"))  ))
+   
     def sitemap = SiteMap(
         home          >> LocGroup("lg1"),
         static,
         twbs,
+        sbL1Dashbord,
+        sbL1Users,
+        sbL2Users,
+        sbL2CreateUsr,
+        sbL2CreateGrp,
+        sbL1Articles,
+        sbL2Articles,
+        sbL2CreateArticle,
         ddLabel1      >> LocGroup("topRight") >> PlaceHolder submenus (
             divider1  >> FoBo.TBLocInfo.Divider >> userMenu
             )
