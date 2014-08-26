c6ca9deb38e79146fc3022462cde5b0b7b970124
karma4u101 <petersson.peter@gmail.com>
2014-08-26 14:07:26 +0200

    fobo-angular tutorial adding scrollspy animation.

--- fobo-angular-sandbox/src/main/webapp/assets/js/appbsutil.js
+++ fobo-angular-sandbox/src/main/webapp/assets/js/appbsutil.js
@@ -34,8 +34,29 @@
           }
         }
       })
-    }, 100)
+    }, 100)    
+
+    //Add some animation to the scrollspy function -- just remove this function if it causes problems.
+    $(".bs-sidebar ul li a[href^='#']").on('click', function(e) {
+
+ 	   // prevent default anchor click behavior
+ 	   e.preventDefault();
+
+ 	   // store hash
+ 	   var hash = this.hash;
+
+ 	   // animate
+ 	   $('html, body').animate({
+ 	       scrollTop: $(this.hash).offset().top
+ 	     }, 500, function(){
+
+ 	       // when done, add hash to url
+ 	       // (default click behaviour)
+ 	       window.location.hash = hash;
+ 	     });
+ 	});      
     
     
   })
 }(window.jQuery)
+
